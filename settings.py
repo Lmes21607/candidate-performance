7145a31be8059c9b9a09d559a1adcb2f40efeaaa
Lmes21607 <85753692+Lmes21607@users.noreply.github.com>
2023-05-12 19:17:27 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -38,14 +38,6 @@ LANGUAGE_CODE = 'en'
 REAL_WORLD_CURRENCY_CODE = 'USD'
 USE_POINTS = True
 
-ROOMS = [
-    dict(
-        name='econ101',
-        display_name='Econ 101 class',
-        participant_label_file='_rooms/econ101.txt',
-    ),
-    dict(name='live_demo', display_name='Room for live demo (no participant labels)'),
-]
 
 ADMIN_USERNAME = 'admin'
 # for security, best to set admin password in an environment variable
