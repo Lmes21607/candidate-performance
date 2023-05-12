717e64c0489593a2e2f6e1a54bcf1782666aaad7
Lmes21607 <85753692+Lmes21607@users.noreply.github.com>
2023-05-12 16:22:05 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -69,17 +69,21 @@ class FirstPage(Page):
 
 class Question(Page):
     form_model = "player"
-    form_fields = ["question_answer"]
+    
     @staticmethod
     def vars_for_template(player: Player):
         question = random.randint(0, 2)
         if question == 0:
-            q = 'Пропустите этот шаг (напишите любую букву в поле)'
+            q = 'Если Вы готовы приступить к эксперименту, нажмите на кнопку ниже'
+            form_fields = []
         elif question == 1:
             q = "На какие факторы Вы бы обратили внимание при выборе кандидата для этого теста, если бы у Вас был доступ ко всем данным кандидатов?"
+            form_fields = ["question_answer"]
         elif question == 2:
             q = "Kак Вы думаете, влияет ли пол на вероятность успеха при выполнении подобных работ?"
-        return {"q":q}
+             form_fields = ["question_answer"]
+        return {"q":q,
+               "form_fields": form_fields}
 
 class Simple_CV(Page):
     form_model = "player"
